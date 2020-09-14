f1eed97a1509d6ee8d004f008af59477e1267f66
Yuki-biu <yuki199908@163.com>
2020-09-15 00:14:13 +0800

    notice

--- miniprogram/components/follow-card/follow-card.js
+++ miniprogram/components/follow-card/follow-card.js
@@ -4,20 +4,27 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    userID: {
+      type: String,
+      value: ''
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    userName: '',
+    avatarUrl: '' 
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    // 通过传入的userID在库中搜索头像和昵称等信息
+    getUserInfo() {
 
+    }
   }
 })
